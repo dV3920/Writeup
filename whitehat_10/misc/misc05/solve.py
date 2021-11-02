b0b69bd9c47d78d92befe061fbbe0fb8f4e54fa8
dV3920 <88095613+dV3920@users.noreply.github.com>
2021-11-02 12:43:20 +0700

    Delete whitehat_10 directory

--- whitehat_10/misc/misc05/solve.py
+++
@@ -1,6 +0,0 @@
-import requests
-key = "XXX"
-response = requests.get("https://discordapp.com/api/v6/guilds/879999382718644234/channels",
-        headers = {"authorization" : key})
-
-print(response.content)
