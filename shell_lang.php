371156bce85afba26944e7e00de962b753bbf486
dV3920 <88095613+dV3920@users.noreply.github.com>
2022-04-10 09:53:37 +0700

    Update shell_lang.php

--- shell_lang.php
+++ shell_lang.php
@@ -1,4 +1,4 @@
-<?php $myfile = fopen("index.php", "r") or die("Unable to open file!");
-            echo fread($myfile,filesize("index.php"));
-            fclose($myfile);
-            ?>
+<?php
+$content = file_get_contents('index.php');
+echo $content;
+?>
